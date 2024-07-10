ac1ffcacd4241118b27cee32be67137edabf5d85
BhargavA29 <bhargavayare@gmail.com>
2024-07-10 19:58:29 +0530

    Added mark as complete or incomplete feature

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import todo_icon from '../assets/todo_icon.png'
 import TodoItems from './TodoItems'
 
@@ -31,7 +31,20 @@ const Todo = () => {
         })
     }
 
+    const toggle = (id)=>{
+        setTodoList((prvTodos)=>{
+            return prvTodos.map((todo)=>{
+                if(todo.id === id ){
+                    return {...todo, isComplete: !todo.isComplete}
+                }
+                return todo;
+            })
+        })
+    }
 
+useEffect(()=>{
+    console.log(todoList);
+},[todoList])
 
   return (
     <div className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7
@@ -52,7 +65,7 @@ const Todo = () => {
 {/*------ todo list --------- */}
     <div className="">
        {todoList.map((item, index)=>{
-            return <TodoItems key={index} text={item.text} id={item.id} isComplete={item.isComplete} deleteTodo={deleteTodo}/>
+            return <TodoItems key={index} text={item.text} id={item.id} isComplete={item.isComplete} deleteTodo={deleteTodo} toggle={toggle}/>
        })}
 
     </div>
