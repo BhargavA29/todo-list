58aedb4c4eb7dede378e99e0705436289e0575eb
BhargavA29 <bhargavayare@gmail.com>
2024-07-10 20:09:42 +0530

    Added Local Storage for the To-Do List Items

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,15 +1,15 @@
-import React, { useEffect, useRef, useState } from 'react'
-import todo_icon from '../assets/todo_icon.png'
-import TodoItems from './TodoItems'
+import React, { useEffect, useRef, useState } from 'react';
+import todo_icon from '../assets/todo_icon.png';
+import TodoItems from './TodoItems';
 
 const Todo = () => {
-
-    const [todoList, setTodoList] = useState([]);
-
+    const [todoList, setTodoList] = useState(
+        localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")) : []
+    );
 
     const inputRef = useRef();
 
-    const add = ()=>{
+    const add = () => {
         const inputText = inputRef.current.value.trim();
         
         if (inputText === "") {
@@ -20,59 +20,70 @@ const Todo = () => {
             id: Date.now(),
             text: inputText,
             isComplete: false,
-        }
-        setTodoList((prev)=>[...prev, newTodo]);
+        };
+        setTodoList((prev) => [...prev, newTodo]);
         inputRef.current.value = "";
-    }
+    };
 
-    const deleteTodo = (id)=>{
-        setTodoList((prvTodos)=>{
-           return prvTodos.filter((todo) => todo.id !== id)
-        })
-    }
+    const deleteTodo = (id) => {
+        setTodoList((prvTodos) => {
+            return prvTodos.filter((todo) => todo.id !== id);
+        });
+    };
 
-    const toggle = (id)=>{
-        setTodoList((prvTodos)=>{
-            return prvTodos.map((todo)=>{
-                if(todo.id === id ){
-                    return {...todo, isComplete: !todo.isComplete}
+    const toggle = (id) => {
+        setTodoList((prvTodos) => {
+            return prvTodos.map((todo) => {
+                if (todo.id === id) {
+                    return { ...todo, isComplete: !todo.isComplete };
                 }
                 return todo;
-            })
-        })
-    }
+            });
+        });
+    };
 
-useEffect(()=>{
-    console.log(todoList);
-},[todoList])
+    useEffect(() => {
+        localStorage.setItem("todos", JSON.stringify(todoList));
+    }, [todoList]);
 
-  return (
-    <div className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7
-    min-h-[550px] rounded-xl'>
+    return (
+        <div className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
+            {/*------ title --------- */}
+            <div className='flex items-center mt-7 gap-2'>
+                <img className='w-8' src={todo_icon} alt="Todo Icon" />
+                <h1 className='text-3xl font-semibold'>To-Do-List</h1>
+            </div>
 
-{/*------ title --------- */}
-    <div className='flex items-center mt-7 gap-2'>
-        <img className='w-8' src={todo_icon} alt="" />
-        <h1 className='text-3xl font-semibold'>To-Do-List</h1>
-    </div>
+            {/*------ input box --------- */}
+            <div className="flex items-center my-7 bg-gray-200 rounded-full">
+                <input
+                    ref={inputRef}
+                    className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600'
+                    type="text"
+                    placeholder='Add your task'
+                />
+                <button onClick={add} className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>
+                    ADD +
+                </button>
+            </div>
 
-{/*------ input box --------- */}
-    <div className="flex items-center my-7 bg-gray-200 rounded-full">
-        <input ref={inputRef} className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600  ' type="text" placeholder='Add your task' />
-        <button onClick={add} className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>ADD +</button>
-    </div>
+            {/*------ todo list --------- */}
+            <div>
+                {todoList.map((item, index) => {
+                    return (
+                        <TodoItems
+                            key={item.id}
+                            text={item.text}
+                            id={item.id}
+                            isComplete={item.isComplete}
+                            deleteTodo={deleteTodo}
+                            toggle={toggle}
+                        />
+                    );
+                })}
+            </div>
+        </div>
+    );
+};
 
-{/*------ todo list --------- */}
-    <div className="">
-       {todoList.map((item, index)=>{
-            return <TodoItems key={index} text={item.text} id={item.id} isComplete={item.isComplete} deleteTodo={deleteTodo} toggle={toggle}/>
-       })}
-
-    </div>
-
-
-    </div>
-  )
-}
-
-export default Todo
+export default Todo;
